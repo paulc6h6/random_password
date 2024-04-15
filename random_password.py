6c4a7bf12146be9ec3decf81c2a6fd6ccf57165c
paulc6h6 <petrapaul@tuta.io>
2024-04-15 23:31:44 +0300

    v0.2

--- random_password.py
+++ random_password.py
@@ -2,23 +2,24 @@ import pyfiglet
 import random
 from colorama import Fore, Back, Style
 
-text = "Password Generator"
-ascii_art = pyfiglet.figlet_format(text)
+title = "Password Generator"
+ascii_art = pyfiglet.figlet_format(title)
 characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+[{]}\|;:"
+
+print(ascii_art)
 
 while True:
     try:
-        length = int(input("How long do you want the password to be ? ( Please enter only a number ) :  "))
+        length = int(input("How long do you want the password to be ? ( Please enter only a number between 6 and 32 characters) :  "))
         if length < 6 or length > 32:
-            print("Ce plm e cu parola asta")
+            print(Fore.RED + "Enter a password length between 6 and 32 characters" + Style.RESET_ALL)
             continue
         randomItems = random.choices(characters, k=length) 
         break
     except ValueError:
-        print("Please enter a number !!!!")
+        print(Fore.RED + "Please enter a number !" + Style.RESET_ALL)
         continue
 
 password = Back.BLACK +  Fore.MAGENTA + "Your password is :" + Fore.RED + "".join(randomItems) + Style.RESET_ALL
 
-print(ascii_art)
 print(password)
