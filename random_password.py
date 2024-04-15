a7fb0df75068af3f7f6b6d193d5532fc06d3f267
paulc6h6 <petrapaul@tuta.io>
2024-04-15 03:02:06 +0300

    v0.1

--- random_password.py
+++ random_password.py
@@ -1,3 +1,14 @@
-name = "Paul"
+import random
 
-print(name)
+characters = "0123456789`~!@#$%^&*()-_=+[{]}|;:,<.>/?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+password_lenght = int(input("Cate caratre doriti sa aiba parola ?"))
+generate = []
+
+for character in characters:
+    generate.extend(random.sample(character, password_lenght))
+
+finnal_password = "".join(generate)
+
+print(f"Your password is : {finnal_password}")
+
+
